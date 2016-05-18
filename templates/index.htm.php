4283582cac3a8c19e6913f8a8a5fbd287e4f1055
Jonas Cosandey <jonascosandey@hotmail.com>
2016-05-18 17:02:29 +0200

    cleaned css

--- templates/index.htm.php
+++ templates/index.htm.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-	<meta charset="utf-8"/>
-	<link rel="stylesheet" type="text/css" href="../css/styles.css"/>
-	<script src="../js/jscript.js"></script>
-	<title>Bilderdatenbank</title>
-</head>
-<body>
-	<div align="center">
-		<table class="top" width="1004" align="center" cellpadding="0" cellspacing="0">
-			<tr>
-				<td class="nav" width="200" valign="top">
-					<?php echo getMenu(getValue(getValue('menu_eintraege')), getValue('menu_titel')); ?>
-				</td>
-				<td class="header" height="20" colspan="2">
-				<?php echo getMetaMenu(getValue(getValue('meta_menu'))); ?>
-				</td>
-			</tr>
-		</table>
-		<?php echo getValue('inhalt');?>
-	</div>
-</body>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="../css/styles.css"/>
+		<script src="../js/jscript.js"></script>
+		<title>Bilderdatenbank</title>
+	</head>
+	<body>
+		<div align="center">
+			<table class="top" width="1004" align="center" cellpadding="0" cellspacing="0">
+				<tr>
+					<td class="nav" width="200" valign="top">
+						<?php echo getMenu(getValue(getValue('menu_eintraege')), getValue('menu_titel')); ?>
+					</td>
+					<td class="header" height="20" colspan="2">
+					<?php echo getMetaMenu(getValue(getValue('meta_menu'))); ?>
+					</td>
+				</tr>
+			</table>
+			<?php echo getValue('inhalt');?>
+		</div>
+	</body>
 </html>
